b879055a8b6d8d3a5f10050c7dbd5a87f3576b97
Guilherme Toshio Miyake <toshio@dinerama.com.br>
2022-04-07 19:29:53 -0300

    typo: meta class prop

--- lazily_typed/__init__.py
+++ lazily_typed/__init__.py
@@ -1,3 +1,3 @@
 from lazily_typed.logger import get_logger, location_info
 from lazily_typed.lazy import Lazy, LazyFactory, LazyContext, lazy_logger, lazy_lambda
-VERSION = "v0.0.1-dev1"
+VERSION = "v0.0.1-dev2"
--- lazily_typed/lazy.py
+++ lazily_typed/lazy.py
@@ -40,7 +40,7 @@ class Lazy(Generic[_T]):
         lazy_logger.debug(f"<{hex(id(self))}> Location  {location_info()}")
 
     def __repr__(self):
-        return f"<{self.__class_.__name__}({self._cls.__name__})>"
+        return f"<{self.__class__.__name__}({self._cls.__name__})>"
 
     @property
     def instance__(self) -> _T:
