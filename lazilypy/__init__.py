a024b2d8038df87754506faeef01d14452150535
Guilherme Toshio Miyake <toshio@dinerama.com.br>
2022-04-09 17:13:52 -0300

    fix: version value

--- lazilypy/__init__.py
+++ lazilypy/__init__.py
@@ -1,4 +1,4 @@
 from lazilypy.logger import get_logger, location_info
 from lazilypy.lazy import Lazy, LazyFactory, LazyContext, lazy_logger, lazy_lambda
 
-VERSION = "v0.0.1-dev2"
+VERSION = "v1.0.0"
